70b9d7879e1666dcf5b9ed4e84b72c19af2a3167
MeliF <MeliF@users.noreply.github.com>
2023-05-28 13:34:10 +0000

    Tokens updated

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,22 +4,22 @@
 //
 
 // Do not edit directly
-// Generated on Sun, 28 May 2023 13:31:26 GMT
+// Generated on Sun, 28 May 2023 13:34:09 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let colorprimary = UIColor(red: 0.510, green: 0.216, blue: 0.988, alpha: 1)
+    public static let colorprimary = UIColor(red: 0.216, green: 0.616, blue: 0.988, alpha: 1)
     public static let colorprimarytext = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
     public static let colorsEmpty = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0)
     public static let colorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
     public static let colorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.10196078431372549)
     public static let colorsRefBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSemiRed = UIColor(red: 0.667, green: 0.141, blue: 0.024, alpha: 1)
+    public static let colorsSemiRed = UIColor(red: 1.000, green: 0.373, blue: 0.016, alpha: 1)
     public static let colorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
     public static let colorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1)
     public static let colorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
-    public static let colorsecondary = UIColor(red: 0.239, green: 0.110, blue: 0.400, alpha: 1)
+    public static let colorsecondary = UIColor(red: 0.176, green: 0.192, blue: 0.525, alpha: 1)
     public static let colorsecondarytext = UIColor(red: 0.851, green: 0.851, blue: 0.851, alpha: 1)
 }
